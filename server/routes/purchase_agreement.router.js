381a53217e015bf61b46e0cf4214a99ca9a34f58
Amran Ahmed <amranahmedd@gmail.com>
2019-07-31 14:08:43 -0500

    purchase_agreement get router created

--- server/routes/purchase_agreement.router.js
+++ server/routes/purchase_agreement.router.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
 /**
  * GET route purchase_contract 
  */
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('GET PURCHASE AGREEMENT  SERVER HIT');
     const queryText = `SELECT * FROM "Purchase_Agreement" ORDER by "id"`;
     pool.query(queryText)
