930c124dac5821457f412b2616c0001fdc8ffe43
Amran Ahmed <amranahmedd@gmail.com>
2019-07-31 14:05:30 -0500

    get router created

--- server/routes/purchase_agreement.router.js
+++ server/routes/purchase_agreement.router.js
@@ -6,7 +6,17 @@ const router = express.Router();
  * GET route purchase_contract 
  */
 router.get('/', (req, res) => {
-    
+    console.log('GET PURCHASE AGREEMENT  SERVER HIT');
+    const queryText = `SELECT * FROM "Purchase_Agreement" ORDER by "id"`;
+    pool.query(queryText)
+        .then(result => {
+            console.log(result.rows);
+            res.send(result.rows)
+        })
+        .catch(error => {
+            console.log('error making SELECT for purchase:', error);
+            res.sendStatus(500);
+        })
 });
 
 /**
