ed6dd4e3ff97a3f72601b60205045356375cafb8
Amran Ahmed <amranahmedd@gmail.com>
2019-07-31 10:33:41 -0500

    navbar created

---
+++ src/components/Drafts/Drafts.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class Drafts extends Component {
+    render() {
+        return (
+            <p>
+                This is the drafts page
+            </p>
+        )
+    }
+}
+export default Drafts;
