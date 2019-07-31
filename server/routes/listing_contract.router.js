87bae5c5722c402a5fdc631880fececf4c4b9c2c
Mang Wan <vang92@gmail.com>
2019-07-31 14:20:08 -0500

    get route for listing contract

---
+++ server/routes/listing_contract.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route listing_contract 
+ */
+router.get('/', (req, res) => {
+    pool.query(`SELECT * FROM "Listing_Contract"`)
+    .then(result => res.send(result.rows))
+    .catch(error => {
+        console.log('error in SONG query', error);
+        res.sendStatus(500);
+    });
+});
+
+/**
+ * POST route listing_contract
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
---
+++ server/routes/purchase_agreement.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route purchase_contract 
+ */
+router.get('/', (req, res) => {
+    
+});
+
+/**
+ * POST route purchase_contract
+ */
+router.post('/', (req, res) => {
+
+});
+
+module.exports = router;
