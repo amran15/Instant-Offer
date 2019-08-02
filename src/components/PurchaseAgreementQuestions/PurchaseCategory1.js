03b23c80e88ed95c17562b51ae76b1b49dd490d3
Mang Wan <vang92@gmail.com>
2019-08-02 09:55:34 -0500

    add material ui

--- src/components/PurchaseAgreementQuestions/PurchaseCategory1.js
+++ src/components/PurchaseAgreementQuestions/PurchaseCategory1.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
-
+//Material UI
+// import Button from '@material-ui/core/Button';
+// import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import swal from 'sweetalert';
+import { TextField } from '@material-ui/core';
 
 
 class PurchaseCategory1 extends Component {
+
     render() {
         return (
             <>
-                This will hold category 1 questions
-                
+                <p>Buyer's Name</p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_name"
+                    value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                />
             </>
         )
     }
