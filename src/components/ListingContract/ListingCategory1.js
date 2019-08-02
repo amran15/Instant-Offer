efbc2de57ff018130efdbeac32660e203277b469
Amran Ahmed <amranahmedd@gmail.com>
2019-08-02 16:44:22 -0500

    hardcoding the listing 1-7 pages

--- src/components/ListingContract/ListingCategory1.js
+++ src/components/ListingContract/ListingCategory1.js
@@ -10,119 +10,37 @@ import InputAdornment from '@material-ui/core/InputAdornment';
 import { TextField } from '@material-ui/core';
 
 class ListingCategory1 extends Component {
+    handleClick = () =>{ 
+        this.props.history.push('/ListingContract')
+    }
     render() {
         return (
-            <>
-            <p>Date</p>
-            <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    minWidth
-                    variant="outlined"
-                    input type="date"
-                />
-                <p><strong>DEFINITIONS:</strong> This Contract involves ths property located at </p>
-                <TextField
-                    // label="Song Title"
-                    id="buyers_name"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    fullWidth
-                    variant="outlined"
-                />
-                <p>Legally described as</p>
-                <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    fullWidth
-                    variant="outlined"
-                /> 
-                <p>Seller's Name </p>
-                <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    fullWidth
-                    variant="outlined"
-                />
-                <p>Broker's Name</p>
-                <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    fullWidth
-                    variant="outlined"
-                />
-                <p>This Contract starts on  </p> <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    minWidth
-                    variant="outlined"
-                    input type="date"
-                /><p>, and ends at 11:59 p.m. on</p>
-                <TextField
-                    // label="Song Title"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    minWidth
-                    variant="outlined"
-                    type="date"
-                />
-                <p>. This Contract terminates upon successful closing of the property(ies) specified in this Contract or expiration or expiration or cancellation of this Contract,<br/>
-                 whichever occurs first.</p>
-                 <p>This Contract may only be canceled by written mutual agreement of the parties.</p>
-                 <p><strong>PRICE:</strong>Seller offers the Property for sale for the price</p>
-                 <TextField
-                    label="$"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    minWidth
-                    variant="outlined"
-                /><p>upon the following terms:</p>
-                  <TextField
-                    // label="$"
-                    id="buyers_earnest_money"
-                    // value={this.state.song_title}
-                    // onChange={this.handleInputChangeFor('song_title')}
-                    fullWidth
-                    variant="outlined"
-                />
-                
-                <Container component="main">
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <div align="left">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                >
-                                    Back
-                        </Button>
-                            </div>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <div align="right">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                >
-                                    Save
-                        </Button>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </>
+            <Container component="main">
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <div align="left" className="Button">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleClick}
+                        >
+                            Back
+                </Button>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div align="right" className="Button">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleClick}
+                        >
+                            Save
+                </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </Container>
         )
     }
 }
--- src/components/ListingContract/ListingCategory2.js
+++ src/components/ListingContract/ListingCategory2.js
@@ -7,64 +7,41 @@ import { withRouter } from 'react-router';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import InputAdornment from '@material-ui/core/InputAdornment';
 import { TextField, Checkbox } from '@material-ui/core';
 
 
-
-
-
 class ListingCategory2 extends Component {
+    handleClick = () =>{ 
+        this.props.history.push('/ListingContract')
+    }
     render() {
         return (
-            <>
-               <p>Option 1. Listing display on the internet.</p>
-               <Checkbox/>
-               <span>Yes<Checkbox/></span>
-               <span>No</span>
-
-               <p>If "No" was selected at Option 1,skip Options 2-4. If "Yes" was selected for option 1, continue to Option 2.</p>
-               <p>Option 2. Listing address(house and unit numbers  and street name) display on the internet.</p>
-               <Checkbox/>
-               <span>Yes<Checkbox/></span>
-               <span>No</span>
-
-               <p>Option 3. An automated valuation of the Property listing or a link to an automated valuation of it may be displayed adjacent to the listing.</p>
-               <Checkbox/>
-               <span>Yes<Checkbox/></span>
-               <span>No</span>
-
-               <p>Option 4. Comments or reviews of the Property by persons other than the displaying broker maybe displayed with or attached as a ling to the listing data of the property.</p>
-               <Checkbox/>
-               <span>Yes<Checkbox/></span>
-               <span>No</span>
-            <br/>
-            <br/>
             <Container component="main">
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <div align="left">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                >
-                                    Back
-                        </Button>
-                            </div>
-                        </Grid>
-                        <Grid item xs={6}>
-                            <div align="right">
-                                <Button
-                                    variant="contained"
-                                    color="primary"
-                                >
-                                    Save
-                        </Button>
-                            </div>
-                        </Grid>
-                    </Grid>
-                </Container>
-            </>
+            <Grid container spacing={2}>
+                <Grid item xs={6}>
+                    <div align="left" className="Button">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleClick}
+                        >
+                            Back
+                </Button>
+                    </div>
+                </Grid>
+                <Grid item xs={6}>
+                    <div align="right" className="Button">
+                        <Button
+                            variant="contained"
+                            color="primary"
+                            onClick={this.handleClick}
+                        >
+                            Save
+                </Button>
+                    </div>
+                </Grid>
+            </Grid>
+        </Container>
         )
     }
 }
