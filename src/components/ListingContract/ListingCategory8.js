c2e7fdc05ffce7b98bfb6566587b8fb361ef84c2
Amran Ahmed <amranahmedd@gmail.com>
2019-08-02 14:00:03 -0500

    hardcoded listing questions for listing contract page 8

--- src/components/ListingContract/ListingCategory8.js
+++ src/components/ListingContract/ListingCategory8.js
@@ -2,16 +2,43 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
+//Material UI 
+import Grid from '@material-ui/core/Grid';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import { TextField } from '@material-ui/core';
 
 
 
 class ListingCategory8 extends Component {
     render() {
         return (
-            <>
-                This will hold category 8 questions
-                
-            </>
+            <div>
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <center>
+                                <h2>Agency Representation</h2>
+                            </center>
+                        </Grid>
+                    </Grid>
+                    <p>Seller's Instructions to Broker:</p>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <Checkbox
+                                value="checkedB"
+                                color="primary"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Checkbox
+                                value="checkedB"
+                                color="primary"
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
         )
     }
 }
