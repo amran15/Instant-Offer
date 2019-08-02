6e33b709ac38ae49ad6604e9ec8fb58bb29c913f
Samuel Titiloye <samuelotitiloye@gmail.com>
2019-08-02 11:45:02 -0500

    listing questions page one completed

--- src/components/ListingContract/ListingCategory1.js
+++ src/components/ListingContract/ListingCategory1.js
@@ -2,15 +2,101 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
-
-
+//Material UI
+// import Button from '@material-ui/core/Button';
+// import Container from '@material-ui/core/Container';
+// import Grid from '@material-ui/core/Grid';
+// import swal from 'sweetalert';
+import { TextField } from '@material-ui/core';
 
 class ListingCategory1 extends Component {
     render() {
         return (
             <>
-                This will hold category 1 questions
-                
+            <p>Date</p>
+            <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    minWidth
+                    variant="outlined"
+                    input type="date"
+                />
+                <p><strong>DEFINITIONS:</strong> This Contract involves ths property located at </p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_name"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                />
+                <p>Legally described as</p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                /> 
+                <p>Seller's Name </p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                />
+                <p>Broker's Name</p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                />
+                <p>This Contract starts on  </p> <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    minWidth
+                    variant="outlined"
+                    input type="date"
+                /><p>, and ends at 11:59 p.m. on</p>
+                <TextField
+                    // label="Song Title"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    minWidth
+                    variant="outlined"
+                    input type="date"
+                />
+                <p>. This Contract terminates upon successful closing of the property(ies) specified in this Contract or expiration or expiration or cancellation of this Contract,<br/>
+                 whichever occurs first.</p>
+                 <p>This Contract may only be canceled by written mutual agreement of the parties.</p>
+                 <p><strong>PRICE:</strong>Seller offers the Property for sale for the price</p>
+                 <TextField
+                    label="$"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    minWidth
+                    variant="outlined"
+                /><p>upon the following terms:</p>
+                  <TextField
+                    // label="$"
+                    id="buyers_earnest_money"
+                    // value={this.state.song_title}
+                    // onChange={this.handleInputChangeFor('song_title')}
+                    fullWidth
+                    variant="outlined"
+                />
             </>
         )
     }
