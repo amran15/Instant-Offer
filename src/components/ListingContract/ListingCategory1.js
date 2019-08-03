974dcc45ba46920fc517d95e55a0e046401e501a
Amran Ahmed <amranahmedd@gmail.com>
2019-08-03 12:50:35 -0500

    removed unused material-ui styling

--- src/components/ListingContract/ListingCategory1.js
+++ src/components/ListingContract/ListingCategory1.js
@@ -6,8 +6,6 @@ import { withRouter } from 'react-router';
 import Button from '@material-ui/core/Button';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import { TextField, InputAdornment } from '@material-ui/core';
 
 class ListingCategory1 extends Component {
@@ -16,73 +14,100 @@ class ListingCategory1 extends Component {
     }
     render() {
         return (
-            <Container component="main">
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <center>
-                            <h2>General Information</h2>
-                        </center>
+            <div>
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <center>
+                                <h2>General Information</h2>
+                            </center>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Property Address</h4>
+                            <TextField
+                                id="street_address"
+                                label="Street Address"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="City"
+                                id="city"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                label="County"
+                                id="county"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Legal description of property</h4>
+                            <TextField
+                                id="legal_description"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Who is the Seller?</h4>
+                            <TextField
+                                id="seller_name"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Who is the Broker?</h4>
+                            <TextField
+                                id="broker_name"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Contract Start Date</h4>
+                            <TextField
+                                id="start_date"
+                                variant="outlined"
+                                type="date"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Contract End Date</h4>
+                            <TextField
+                                id="end_date"
+                                variant="outlined"
+                                type="date"
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>What is the property price?</h4>
+                            <TextField
+                                id="property_price"
+                                fullWidth
+                                variant="outlined"
+                                InputProps={{
+                                    startAdornment: <InputAdornment position="start">$</InputAdornment>,
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Additional Terms</h4>
+                            <TextField
+                                id="following_terms"
+                                fullWidth
+                                variant="outlined"
+                            />
+                        </Grid>
                     </Grid>
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>What is the property address?</h4>
-                    <TextField
-                        id="property_address"
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>Who is the Seller?</h4>
-                    <TextField
-                        id="seller_name"
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>Who is the Broker?</h4>
-                    <TextField
-                        id="broker_name"
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>Contract Start Date</h4>
-                    <TextField
-                        id="start_date"
-                        variant="outlined"
-                        type="date"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>Contract End Date</h4>
-                    <TextField
-                        id="end_date"
-                        variant="outlined"
-                        type="date"
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>What is the property price?</h4>
-                    <TextField
-                        id="property_price"
-                        fullWidth
-                        variant="outlined"
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start">$</InputAdornment>,
-                        }}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <h4>Additional Terms</h4>
-                    <TextField
-                        id="following_terms"
-                        fullWidth
-                        variant="outlined"
-                    />
-                </Grid>
+                </Container>
                 <br />
                 <br />
                 <Container component="main">
@@ -111,7 +136,7 @@ class ListingCategory1 extends Component {
                         </Grid>
                     </Grid>
                 </Container>
-            </Container>
+            </div>
         )
     }
 }
--- src/components/ListingContract/ListingCategory7.js
+++ src/components/ListingContract/ListingCategory7.js
@@ -5,44 +5,76 @@ import { withRouter } from 'react-router';
 import Grid from '@material-ui/core/Grid';
 import Checkbox from '@material-ui/core/Checkbox';
 import Container from '@material-ui/core/Container';
-import { TextField } from '@material-ui/core';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Button from '@material-ui/core/Button';
 
 
 
 class ListingCategory7 extends Component {
-    handleClick = () =>{ 
+    handleClick = () => {
         this.props.history.push('/ListingContract')
     }
     render() {
         return (
-            <Container component="main">
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <div align="left" className="Button">
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={this.handleClick}
-                        >
-                            Back
+            <div>
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <center>
+                                <h2>Foreign Investment</h2>
+                            </center>
+                        </Grid>
+                        <h4>Seller represents and warrants that seller is a foreign person for purpose of income taxation</h4>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value="checkedB"
+                                        color="primary" />
+                                }
+                                label="Yes"
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        value="checkedB"
+                                        color="primary" />
+                                }
+                                label="No"
+                            />
+                        </Grid>
+                        <p>Due to the complexity and potential risks of failing to comply with FIRPTA, Seller should <b>seek appropiate legal and tax advice regarding FIRPTA compliance, as Broker will be unable to confirm whether Seeker is a foreign person or whether the withholding requirements of FIRPTA apply.</b> </p>
+                    </Grid>
+                </Container>
+                <br />
+                <br />
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <div align="left" className="Button">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleClick}
+                                >
+                                    Back
                 </Button>
-                    </div>
-                </Grid>
-                <Grid item xs={6}>
-                    <div align="right" className="Button">
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            onClick={this.handleClick}
-                        >
-                            Save
+                            </div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div align="right" className="Button">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleClick}
+                                >
+                                    Save
                 </Button>
-                    </div>
-                </Grid>
-            </Grid>
-        </Container>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div >
         )
     }
 }
