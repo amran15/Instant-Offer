cd6183bddcd12034f5252ad953b3a488b1adf7b0
Mang Wan <vang92@gmail.com>
2019-08-04 00:14:08 -0500

    completed category 7 questions

--- src/components/PurchaseAgreementQuestions/PurchaseCategory7.js
+++ src/components/PurchaseAgreementQuestions/PurchaseCategory7.js
@@ -2,13 +2,87 @@ import React, { Component } from "react";
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router';
 
+//Material UI
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Checkbox from '@material-ui/core/Checkbox';
+import Container from '@material-ui/core/Container';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { TextField } from '@material-ui/core';
 
 class PurchaseCategory7 extends Component {
     render() {
         return (
-            <>
-                This will hold category 7 questions
-            </>
+            <div>
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <center>
+                                <h2>Seller's Contributions to Buyer's Costs</h2>
+                            </center>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <h4>Please check one.</h4>
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        // checked={state.checkedB}
+                                        // onChange={handleChange('checkedB')}
+                                        value="checkedB"
+                                        color="primary"
+                                    />}
+                                label="This Purchase Agreement is subject to an Addendum to Purchase Agreement: Sale of Buyer's Property Contingency for the sale of Buyer's property. (If checked, see attached Addendum."
+                            />
+                            <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        // checked={state.checkedB}
+                                        // onChange={handleChange('checkedB')}
+                                        value="checkedB"
+                                        color="primary"
+                                    />}
+                                label="This Purchase Agreement is contingent upon the succesful closing on the Buyer's property."
+                            />
+                             <FormControlLabel
+                                control={
+                                    <Checkbox
+                                        // checked={state.checkedB}
+                                        // onChange={handleChange('checkedB')}
+                                        value="checkedB"
+                                        color="primary"
+                                    />}
+                                label="Buyer represents that Buyer has the financial ability to perform on this Purchase Agreement without the sale and closing on any other property."
+                            />
+                        </Grid>
+                    </Grid>
+                </Container>
+                <br/>
+                <Container component="main">
+                    <Grid container spacing={2}>
+                        <Grid item xs={6}>
+                            <div align="left">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Back
+                        </Button>
+                            </div>
+                        </Grid>
+                        <Grid item xs={6}>
+                            <div align="right">
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                >
+                                    Save
+                        </Button>
+                            </div>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </div>
         )
     }
 }
