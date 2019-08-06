23f44d41d49ad882af1099bf5b6000719f49e346
Samuel Titiloye <samuelotitiloye@gmail.com>
2019-08-06 11:57:42 -0500

    fixed sagas conflict

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -5,19 +5,19 @@ import userSaga from './userSaga';
 
 import fetchListingAnswers from './fetchListingAnswers';
 
-<<<<<<< HEAD
-import pdfListing from './pdfListing'
-import pdfOffer from './pdfOffer'
+
+// import pdfListing from './pdfListing'
+// import pdfOffer from './pdfOffer'
 import listingCategorySaga from './listingcategorysaga';
 // 
-=======
+
 import pdfListing from './pdfListing';
 import pdfOffer from './pdfOffer';
 
 import deleteListingAnswer from './deleteListingAnswer';
 import deletePurchaseAnswerSaga from './deletePurchaseAnswer';
 
->>>>>>> 28010e862fc90d6a20ffa289fd1f865d8a81d7af
+
 
 // rootSaga is the primary saga.
 // It bundles up all of the other sagas so our project can use them.
@@ -34,11 +34,8 @@ export default function* rootSaga() {
     fetchListingAnswers(),
     pdfListing(),
     pdfOffer(),
-<<<<<<< HEAD
     listingCategorySaga(),
-=======
     deleteListingAnswer(),
-    deletePurchaseAnswerSaga()
->>>>>>> 28010e862fc90d6a20ffa289fd1f865d8a81d7af
+    deletePurchaseAnswerSaga(),
   ]);
 }
