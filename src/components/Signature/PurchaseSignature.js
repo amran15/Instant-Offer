3225474cf3d2d2e2822b0f08ab710edfc38de5f6
Mang Wan <vang92@gmail.com>
2019-08-12 17:41:26 -0500

    fixed second signature page

--- src/components/Signature/PurchaseSignature.js
+++ src/components/Signature/PurchaseSignature.js
@@ -5,16 +5,19 @@ import SignatureCanvas, { canvas } from 'react-signature-canvas';
 
 //Material UI       
 import Button from '@material-ui/core/Button';
+import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import swal from 'sweetalert';
 
 
 const styles = {
     Container: {
-        marginTop: '465px',
+        marginTop: '10px',
     },
+    button: {
+        color: 'white',
+      },
 };
-
 
 
 class PurchaseSignature extends Component {
@@ -23,7 +26,6 @@ class PurchaseSignature extends Component {
         answers:
         {SIGNATURE_BUYER_1: null}
     }
-
     sigPad = {}
 
     //this will clear the signature if a mistake was made when signing
@@ -36,27 +38,24 @@ class PurchaseSignature extends Component {
         this.props.history.push('/ListingContract')
     }
 
-    //this will trim down the signature and get rid of the white space
-    //it will capture the signature and save it as image with a png extension
-    trimSignature = () => {
-        this.setState({
-            answers: {
-                SIGNATURE_BUYER_1: this.sigPad.getTrimmedCanvas().toDataURL('image/PNG')
-            }
-        })
-    }
-
     //this will save the signature once it has been trimmed
     handleClickToSaveSignature = () => {
         //POST_SIGNATURE action goes to the sagas with a generator function sendSignatureToDatabase
-        this.props.dispatch({ type: 'SAVE_ANSWERS', payload: this.state })
-        this.trimSignature();
+        this.setState({
+            answers: {
+                SIGNATURE_BUYER_1: this.sigPad.getTrimmedCanvas().toDataURL('image/PNG')
+            },
+        }, () => {
+            console.log('state goes here', this.state);
+            this.props.dispatch({ type: 'SAVE_ANSWERS', payload: this.state });
+        })
+
         swal({
             title: "Signature Saved!",
             text: "",
             icon: "success",
             button: "Ok",
-        }).then(() => { 
+        }).then(() => {
             this.props.history.push('/signed-documents')
         })
     }
@@ -67,19 +66,16 @@ class PurchaseSignature extends Component {
             <div>
                 <div className="sig-canvas">
                     <SignatureCanvas penColor="black"
-                        canvasProps={{ width:390, height: 750, className: 'sigCanvas' }}
+                        canvasProps={{ width:390, height: 525, className: 'sigCanvas' }}
                         ref={(ref) => { this.sigPad = ref }} />
                     {
                         SIGNATURE_BUYER_1 ? <img src={SIGNATURE_BUYER_1} alt="" /> : null
                     }
                 </div>
-                {/* <Container
-                    // component="main"
-                    // style={styles.Container}
-                > */}
+                <center>
+                <Container style={styles.Container}>
                     <Grid container spacing={1}>
-                        <Grid item xs={3}>
-
+                        <Grid item xs={4}>
                             <Button
                                 fullWidth
                                 variant="contained"
@@ -89,7 +85,7 @@ class PurchaseSignature extends Component {
                                 Back
                             </Button>
                         </Grid>
-                        <Grid item xs={3}>
+                        <Grid item xs={4}>
                             <Button
                                 fullWidth
                                 variant="contained"
@@ -99,17 +95,7 @@ class PurchaseSignature extends Component {
                                 Clear
                                 </Button>
                         </Grid>
-                        <Grid item xs={3}>
-                            <Button
-                                fullWidth
-                                variant="contained"
-                                color="primary"
-                                onClick={this.trimSignature}
-                            >
-                                Trim
-                                </Button>
-                        </Grid>
-                        <Grid item xs={3}>
+                        <Grid item xs={4}>
                             <Button
                                 fullWidth
                                 variant="contained"
@@ -120,7 +106,8 @@ class PurchaseSignature extends Component {
                             </Button>
                         </Grid>
                     </Grid>
-                {/* </Container> */}
+                    </Container>
+                    </center>
             </div>
         )
     }
