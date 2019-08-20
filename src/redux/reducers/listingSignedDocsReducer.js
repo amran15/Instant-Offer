dce239696b2a99681562d08cd0035e420c2e2400
Joseph Wan <jhwanus@us.ibm.com>
2019-08-20 14:17:38 -0500

    feat: installed material-ui/icons, eslint config file; set up delete for listing contract

--- src/redux/reducers/listingSignedDocsReducer.js
+++ src/redux/reducers/listingSignedDocsReducer.js
@@ -3,6 +3,7 @@ const listingSignedDocsReducer = (state = [], action) => {
     case 'SET_LISTING_SIGNED_DOCS':
       return action.payload
     case 'DELETE_LISTING_CONTRACT':
+      console.log('inDeleteListingContract')
       return state.filter((value, index, arr) => {
         return value.id != action.payload;
       })
---
+++ src/redux/sagas/deletePurchaseDoc.js
@@ -0,0 +1,20 @@
+import { put, takeLatest } from 'redux-saga/effects';
+import axios from 'axios';
+
+//const only needed for receiving information back 
+function* deletePurchaseDoc(action) {
+    try {
+      console.log('deleting purchase doc')
+       yield axios.delete(`/api/listing/delete/${action.payload.id}`);
+       yield put ({ type: 'DELETE_PURCHASE_AGREEMENT', payload: action.payload.id});
+    } catch (error) {
+        console.log('error deleting purchase_agreement for pdf', error);
+    }
+}
+
+function* deletePurchaseDocSaga() {
+  yield takeLatest('DELETE_PURCHASE_DOC', deletePurchaseDoc);
+}
+
+export default deletePurchaseDocSaga;
+
