8210b2fc59423fb8310bf1a4d014b74115fd68c9
Samuel Titiloye <samuelotitiloye@gmail.com>
2019-08-26 10:05:17 -0500

    added missing columns to the db while testing purchase

--- src/components/PurchaseAgreementQuestions/PurchaseCategory2.js
+++ src/components/PurchaseAgreementQuestions/PurchaseCategory2.js
@@ -47,6 +47,9 @@ class PurchaseCategory2 extends Component {
     render() {
         return (
             <div>
+                <pre>
+                    {JSON.stringify(this.state, null, 2)}
+                </pre>
                 <Container component="main">
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
