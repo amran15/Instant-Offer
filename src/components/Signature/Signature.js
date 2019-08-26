b7e74f37a38a16ea0b8d5cc34986fc02214c26a8
Joseph Wan <jhwanus@us.ibm.com>
2019-08-26 18:21:41 -0500

    feat: added border to canvas to make clear where we're signing

--- src/components/Signature/Signature.js
+++ src/components/Signature/Signature.js
@@ -3,6 +3,7 @@ import { Component } from 'react';
 import { connect } from 'react-redux';
 import SignatureCanvas  from 'react-signature-canvas';
 import MediaQuery from 'react-responsive';
+import './Signature.css'
 
 //Material UI
 import Button from '@material-ui/core/Button';
