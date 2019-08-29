73096d93ac4779cb696da22a3aa996cddb46038a
Mang Wan <vang92@gmail.com>
2019-08-29 00:54:45 -0500

    removed json from purchase categories

--- src/components/PurchaseAgreementQuestions/PurchaseCategory7.js
+++ src/components/PurchaseAgreementQuestions/PurchaseCategory7.js
@@ -44,9 +44,6 @@ class PurchaseCategory7 extends Component {
     render() {
         return (
             <div>
-                <pre>
-                    {JSON.stringify(this.state, null, 2)}
-                </pre>
                 <Container component="main">
                     <Grid container spacing={2}>
                         <Grid item xs={12}>
