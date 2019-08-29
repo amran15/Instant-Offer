e5635a1aef95d2b050099f60a78e56bcc2033144
Joseph Wan <jhwanus@us.ibm.com>
2019-08-29 03:03:47 -0500

    feat: change name of draft according to docType

--- src/components/IndividualDrafts/IndividualDrafts.js
+++ src/components/IndividualDrafts/IndividualDrafts.js
@@ -61,7 +61,12 @@ class IndividualDrafts extends Component {
               <Grid item xs={12} container spacing={3}>
                 <Grid item xs={9}>
                   <div style={styles.title}>
-                    <h2>{draft.BUYER_1}'s Contract</h2>
+                    <h2>{draft.BUYER_1}'s {this.props.docType === 'listing' ?
+                    'Listing Contract Draft'
+                    :
+                    'Purchase Agreement Draft'
+                }
+              </h2>
                   </div>
                 </Grid>
                 <Grid
